68725ce628d32461227d9763bf5a4eaf90a436d9
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-07-14 23:31:10 -0400

    Increased particles

--- circular_animation.js
+++ circular_animation.js
@@ -64,7 +64,7 @@ function particle(x, y, radius, color) {
 let particles
 function init() {
 	particles = [];
-	for (var i = 0; i < 100; i++) {
+	for (var i = 0; i < 150; i++) {
 		const radius = (Math.random() * 2) +1;
 		particles.push(new particle(canvas.width/2, canvas.height/2, num_in_range(2, 6), randColor(particle_colors)))
 	}
